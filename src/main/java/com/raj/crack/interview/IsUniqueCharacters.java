f2627e24d941115a90d00f5b24b83aa4fbd484a9
rajeshso <fun_rajesh@yahoo.com>
2020-02-09 16:51:05 +0000

    IsUniqueCharacters

--- src/main/java/com/raj/crack/interview/IsUniqueCharacters.java
+++ src/main/java/com/raj/crack/interview/IsUniqueCharacters.java
@@ -1,7 +1,26 @@
 package com.raj.crack.interview;
-
+/**
+ * Page 90
+ * Cracking the Coding Interview, 6th Edition
+ * Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+ */
 public class IsUniqueCharacters {
     public boolean isUnique(String word) {
-        return true;
+        char[] chars = word.toCharArray();
+        char compareChar = 0;
+        int length = chars.length;
+        boolean result = true;
+        outerloop:
+        for (int i = 0; i <= length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                System.out.printf("i %d is %s and j %d is %s\n", i, chars[i], j, chars[j]);
+                if (chars[i] == chars[j]) {
+                    result = false;
+                    break outerloop;
+                }
+            }
+
+        }
+        return result;
     }
 }
--- src/test/java/com/raj/crack/interview/TestIsUnique.java
+++ src/test/java/com/raj/crack/interview/TestIsUnique.java
@@ -1,4 +1,34 @@
 package com.raj.crack.interview;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Page 90
+ * Cracking the Coding Interview, 6th Edition
+ * Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+ */
 public class TestIsUnique {
+    private IsUniqueCharacters isUniqueCharacters = new IsUniqueCharacters();
+
+    @Test
+    public void testIsUniqueTrueIfUnique() {
+        String word = "word";
+        assertTrue(isUniqueCharacters.isUnique(word), "The characters are unique");
+    }
+
+    @Test
+    public void testIsUniqueFalseIfNotUnique() {
+        String word = "palace";
+        assertFalse(isUniqueCharacters.isUnique(word), "The characters palace are not unique");
+    }
+
+    @Test
+    public void testIsUniqueFalseIfonlyDuplicates() {
+        String word = "aa";
+        assertFalse(isUniqueCharacters.isUnique(word), "The characters aa are not unique");
+    }
+
 }
