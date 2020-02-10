2d0b6d540880cd98970f7fd8c0b1acfd4ba0602e
rajeshsomasundaram <rajesh.somasundaram@10xbanking.com>
2020-02-10 18:33:42 +0000

    test

--- src/main/java/com/raj/crack/interview/RomanNumeral.java
+++ src/main/java/com/raj/crack/interview/RomanNumeral.java
@@ -1,10 +1,36 @@
 package com.raj.crack.interview;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class RomanNumeral {
 
   private final static TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+
+  enum RomanNumeralEnum {
+    I(1), IV(4), V(5), IX(9), X(10),
+    XL(40), L(50), XC(90), C(100),
+    CD(400), D(500), CM(900), M(1000);
+
+    private int value;
+
+    RomanNumeralEnum(int value) {
+      this.value = value;
+    }
+
+    public int getValue() {
+      return value;
+    }
+
+    public static List<RomanNumeralEnum> getReverseSortedValues() {
+      return Arrays.stream(values())
+          .sorted(Comparator.comparing((RomanNumeralEnum e) -> e.value).reversed())
+          .collect(Collectors.toList());
+    }
+  }
 
   static {
 
@@ -24,12 +50,31 @@ public class RomanNumeral {
 
   }
 
-  public String toRoman1(int number) {
-    int floorKey = map.floorKey(number);
-    if (floorKey == number) {
-      return map.get(number);
+
+
+  public static int toArabic(String input) {
+    String romanNumeral = input.toUpperCase();
+    int result = 0;
+
+    List<RomanNumeralEnum> romanNumerals = RomanNumeralEnum.getReverseSortedValues();
+
+    int i = 0;
+
+    while ((romanNumeral.length() > 0) && (i < romanNumerals.size())) {
+      RomanNumeralEnum symbol = romanNumerals.get(i);
+      if (romanNumeral.startsWith(symbol.name())) {
+        result += symbol.getValue();
+        romanNumeral = romanNumeral.substring(symbol.name().length());
+      } else {
+        i++;
+      }
     }
-    return map.get(floorKey) + toRoman1(number - floorKey);
+
+    if (romanNumeral.length() > 0) {
+      throw new IllegalArgumentException(input + " cannot be converted to a Roman Numeral");
+    }
+
+    return result;
   }
 
 
@@ -42,7 +87,8 @@ public class RomanNumeral {
   }
 
   public static void main(String[] args) {
-    System.out.println(toRoman(4200));
+  //  System.out.println(toRoman(4200));
+    System.out.println(toArabic("MMMCC"));
   }
 
 }
